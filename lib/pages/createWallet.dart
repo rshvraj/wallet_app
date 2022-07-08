9d7e616a5b797b307f98cc85ca7dff82f3cd2143
Rishabh <rshvraj36@gmail.com>
2022-07-08 19:09:53 +0530

    output

---
+++ lib/pages/createWallet.dart
@@ -0,0 +1,164 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+import '../utilities/firestore.dart';
+import '../utilities/walletCreation.dart';
+
+class CreateWallet extends StatefulWidget {
+  const CreateWallet({Key? key}) : super(key: key);
+
+  @override
+  State<CreateWallet> createState() => _CreateWallet();
+}
+
+class _CreateWallet extends State<CreateWallet> {
+  int? selected;
+  String? pubAddress;
+  String? privAddress;
+  String? username;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    details();
+  }
+
+  details() async {
+    dynamic data = await getUserDetails();
+    data != null
+        ? setState(() {
+            privAddress = data['privateKey'];
+            pubAddress = data['publicKey'];
+            username = data['user_name'];
+            bool created = data['wallet_created'];
+            created == true ? selected = 1 : selected = 0;
+          })
+        : setState(() {
+            selected = 0;
+          });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Wallet Information"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.backspace),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          selected == 0
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      child: const Text("Add a Wallet"),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.all(20),
+                        child: ElevatedButton(
+                          child: const Icon(Icons.add),
+                          onPressed: () async {
+                            setState(() {
+                              selected = 1;
+                            });
+                            WalletAddress service = WalletAddress();
+                            final mnemonic = service.generateMnemonic();
+                            final privateKey =
+                                await service.getPrivateKey(mnemonic);
+                            final publicKey =
+                                await service.getPublicKey(privateKey);
+                            privAddress = privateKey;
+                            pubAddress = publicKey.toString();
+                            addUserDetails(privateKey, publicKey);
+                          },
+                        ))
+                  ],
+                )
+              : Column(
+                  children: [
+                    Center(
+                        child: Container(
+                      margin: const EdgeInsets.all(20),
+                      alignment: Alignment.center,
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.blueAccent,
+                      child: const Text(
+                        "Successfully initiated wallet!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    )),
+                    const Center(
+                      child: Text(
+                        "Wallet Private Address :",
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "${privAddress}",
+                        style: const TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        "Do not reveal your private address to anyone!",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const Divider(),
+                    const Center(
+                      child: Text(
+                        "Wallet Public Address : ",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "${pubAddress}",
+                        style: const TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
+                    const Divider(),
+                    const Center(
+                      child: Text(
+                        "Go back to main page!",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/welcomePage.dart
@@ -0,0 +1,217 @@
+// ignore_for_file: file_names
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart';
+import 'package:flutter/services.dart';
+
+import '../utilities/firestore.dart';
+import 'createWallet.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Client httpClient;
+  late Web3Client ethClient;
+  String privAddress = "";
+  EthereumAddress targetAddress =
+      EthereumAddress.fromHex("0xfed0d2b05602d8b3b0fe5b7eb80f124ee98013cd");
+  bool? created;
+  var balance;
+  var credentials;
+  int myAmount = 5;
+  var pro_pic;
+  var u_name;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    httpClient = Client();
+    ethClient = Web3Client(
+        "https://rinkeby.infura.io/v3/4c98357a2edc4fce82379fe98baa9f97",
+        httpClient);
+    details();
+  }
+
+  details() async {
+    dynamic data = await getUserDetails();
+    data != null
+        ? setState(() {
+            privAddress = data['privateKey'];
+            var publicAdress = data['publicKey'];
+            var temp = EthPrivateKey.fromHex(privAddress);
+            credentials = temp.address;
+            // EthPrivateKey fromHex = EthPrivateKey.fromHex(privAddress);
+            // final password = FirebaseAuth.instance.currentUser!.uid;
+            // Random random=Random.secure();
+            // Wallet wallet = Wallet.createNew(fromHex, password,random);
+            // print(wallet.toJson());
+            created = data['wallet_created'];
+            balance = getBalance(credentials);
+          })
+        : print("Data is NULL!");
+  }
+
+  Future<DeployedContract> loadContract() async {
+    String abi = await rootBundle.loadString("assets/abi/abi.json");
+    String contractAddress = "0x4a151003126f41c5cb23e31f5f29da05676e9cae";
+    final contract = DeployedContract(ContractAbi.fromJson(abi, "Gold"),
+        EthereumAddress.fromHex(contractAddress));
+    return contract;
+  }
+
+  Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
+    final contract = await loadContract();
+    final ethFunction = contract.function(functionName);
+    final result = await ethClient.call(
+        contract: contract, function: ethFunction, params: args);
+    return result;
+  }
+
+  Future<void> getBalance(EthereumAddress credentialAddress) async {
+    List<dynamic> result = await query("balanceOf", [credentialAddress]);
+    var data = result[0];
+    setState(() {
+      balance = data;
+    });
+  }
+
+  Future<String> sendCoin() async {
+    var bigAmount = BigInt.from(myAmount);
+    var response = await submit("transfer", [targetAddress, bigAmount]);
+    return response;
+  }
+
+  Future<String> submit(String functionName, List<dynamic> args) async {
+    DeployedContract contract = await loadContract();
+    final ethFunction = contract.function(functionName);
+    EthPrivateKey key = EthPrivateKey.fromHex(privAddress);
+    Transaction transaction = await Transaction.callContract(
+        contract: contract,
+        function: ethFunction,
+        parameters: args,
+        maxGas: 100000);
+    print(transaction.nonce);
+    final result =
+        await ethClient.sendTransaction(key, transaction, chainId: 4);
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user?.photoURL == null) {
+      pro_pic = "assets/images/logo.png";
+    } else {
+      pro_pic = user!.photoURL;
+    }
+    if (user?.displayName == null) {
+      u_name = "User Name";
+    } else {
+      u_name = user!.displayName;
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              color: Colors.blue[600],
+              height: 150,
+              alignment: Alignment.center,
+              child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      image: DecorationImage(
+                          image: NetworkImage(pro_pic), scale: 0.1))),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: Text(
+                u_name,
+                style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              alignment: Alignment.center,
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.black,
+              child: const Text(
+                "Balance",
+                style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              alignment: Alignment.center,
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.black,
+              child: Text(
+                balance == null ? "0 GLD" : "${balance}\GLD",
+                style: const TextStyle(
+                    fontSize: 50,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    var response = await sendCoin();
+                    print(response);
+                  },
+                  child: const Text("Send Money"),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                )),
+            Container(
+                margin: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    credentials != null
+                        ? getBalance(credentials)
+                        : print("credentials null");
+                  },
+                  child: const Text("Refresh Page"),
+                )),
+            Container(
+              margin: const EdgeInsets.only(top: 30, right: 30),
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const CreateWallet()));
+                },
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ],
+        ));
+  }
+}
